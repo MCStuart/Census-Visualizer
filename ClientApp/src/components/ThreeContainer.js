e1ff7d45018b7dad77d9fd81990d279dd7511a0f
Not Real and Stuart McKay <not+stuart@>
2019-07-29 16:51:52 -0700

    add low-poly US states' models, three.js nonfunctional

--- ClientApp/src/components/ThreeContainer.js
+++ ClientApp/src/components/ThreeContainer.js
@@ -1,59 +1,52 @@
 import React, { Component } from 'react';
-import * as THREE from 'three';
-class ThreeScene extends Component{
+import * as THREE from '../threejs/js/three';
+
+import './ThreeContainer.css';
+
+class ThreeContainer extends Component{
+
   componentDidMount(){
-    const width = this.mount.clientWidth
-    const height = this.mount.clientHeight
-    //ADD SCENE
-    this.scene = new THREE.Scene()
-    //ADD CAMERA
-    this.camera = new THREE.PerspectiveCamera(
-      75,
-      width / height,
-      0.1,
-      1000
-    )
-    this.camera.position.z = 4
-    //ADD RENDERER
-    this.renderer = new THREE.WebGLRenderer({ antialias: true })
-    this.renderer.setClearColor('#000000')
-    this.renderer.setSize(width, height)
-    this.mount.appendChild(this.renderer.domElement)
-    //ADD CUBE
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshBasicMaterial({ color: '#433F81'     })
-    this.cube = new THREE.Mesh(geometry, material)
-    this.scene.add(this.cube)
-this.start()
-  }
-componentWillUnmount(){
-    this.stop()
-    this.mount.removeChild(this.renderer.domElement)
-  }
-start = () => {
-    if (!this.frameId) {
-      this.frameId = requestAnimationFrame(this.animate)
+    const three = THREE;
+
+    const scene = new THREE.Scene();
+
+    const fov = 45;
+    const aspect = window.innerWidth / window.innerHeight;
+    // If aspect doesn't work, replace with " width / height "  
+    // const width = window.innerWidth;
+    // const height = window.innerHeight;
+    const near = 0.1;
+    const far = 1000;
+
+    const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidith, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+    
+    // logic goes here
+    const update = () => {};
+
+    // draw scene
+    const render = () => {
+      renderer.render( scene, camera );
     }
+
+    //run game loop (update, render, repeat)
+    const GameLoop = () => {
+      requestAnimationFrame( GameLoop );
+
+      update();
+      render();
+    };
+
+    GameLoop();
   }
-stop = () => {
-    cancelAnimationFrame(this.frameId)
-  }
-animate = () => {
-   this.cube.rotation.x += 0.01
-   this.cube.rotation.y += 0.01
-   this.renderScene()
-   this.frameId = window.requestAnimationFrame(this.animate)
- }
-renderScene = () => {
-  this.renderer.render(this.scene, this.camera)
-}
-render(){
-    return(
-      <div
-        style={{ width: '700px', height: '700px' }}
-        ref={(mount) => { this.mount = mount }}
-      />
-    )
+
+  render() {
+      return(
+        <div />
+      )
   }
 }
-export default ThreeScene
+export default ThreeContainer
