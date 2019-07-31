096583ddbf0ce5b0b74608bc3d78150c368eb1dc
Student <example@example.com>
2019-07-31 01:50:08 -0700

    add lighting to scene

--- ClientApp/src/components/ThreeContainer.js
+++ ClientApp/src/components/ThreeContainer.js
@@ -9,7 +9,6 @@ import * as url from '../assets/MapModels/usa_map_-_low_poly/scene.glb';
 
 class ThreeContainer extends Component {
     componentDidMount(){
-      console.log(url);
       
       const width = window.innerWidth;
       const height = window.innerHeight;
@@ -17,12 +16,14 @@ class ThreeContainer extends Component {
       this.scene = new THREE.Scene()
       //ADD CAMERA
       this.camera = new THREE.PerspectiveCamera(
-        75,
+        90,
         width / height,
         0.1,
         1000
       )
-      this.camera.position.z = 4
+      this.camera.position.x = 0;
+      this.camera.position.y = 3;
+      this.camera.position.z = 4;
       //ADD RENDERER
       this.renderer = new THREE.WebGLRenderer({ antialias: true })
       this.renderer.setClearColor('aquamarine')
@@ -30,6 +31,18 @@ class ThreeContainer extends Component {
       this.renderer.gammaOutput = true;
       this.renderer.gammaFactor = 2.2;
       this.mount.appendChild(this.renderer.domElement)
+
+      //LIGHTNING
+      //first point light
+      this.light = new THREE.PointLight(0xffffff, 1, 4000);
+      this.light.position.set(50, 0, 0);
+      //the second one
+      this.light_two = new THREE.PointLight(0xffffff, 1, 4000);
+      this.light_two.position.set(-100, 800, 800);
+      //And another global lightning some sort of cripple GL
+      this.lightAmbient = new THREE.AmbientLight(0x404040);
+      this.scene.add(this.light, this.light_two, this.lightAmbient);
+    
       //ADD CUBE
       // const geometry = new THREE.BoxGeometry(1, 1, 1)
       // const material = new THREE.MeshBasicMaterial({ color: 'pink' })
@@ -40,10 +53,13 @@ class ThreeContainer extends Component {
       // ADD MODEL  
       var loader = new GLTFLoader();
       loader.load(url, ( gltf ) => {
+        console.log(gltf);
+        
+        
         const root = gltf.scene;
+
         this.scene.add(root);
       });
-
 
       // ADD ORBIT CONTROLLS
       this.controls = new OrbitControls(this.camera, this.renderer.domElement);
