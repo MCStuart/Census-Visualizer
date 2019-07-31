fd79dc187730a14c39c348f0e182542ace38fcf6
Student <example@example.com>
2019-07-31 09:26:13 -0700

    start to set up C# data models with mySQL

--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -60,7 +64,7 @@ namespace census_map_visualizer
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseProxyToSpaDevelopmentServer("http://localhost:8000");                }
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:3000");                }
             });
         }
     }
